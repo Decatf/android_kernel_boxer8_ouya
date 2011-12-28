57f43bb55dbf2212b851feb1d232943e7fa354a6
Aaron Lu <aaron.lu@amd.com>
2011-12-28 11:11:12 +0800

    mmc: sdhci: Fix tuning timer incorrect setting when suspending host
    When suspending host, the tuning timer shoule be deactivated.
    And the HOST_NEEDS_TUNING flag should be set after tuning timer is
    deactivated.
    
    Signed-off-by: Philip Rakity <prakity@marvell.com>
    Signed-off-by: Aaron Lu <aaron.lu@amd.com>
    Acked-by: Adrian Hunter <adrian.hunter@intel.com>
    Cc: stable <stable@vger.kernel.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -2330,9 +2330,8 @@ int sdhci_suspend_host(struct sdhci_host *host, pm_message_t state)
 	has_tuning_timer = host->version >= SDHCI_SPEC_300 &&
 		host->tuning_count && host->tuning_mode == SDHCI_TUNING_MODE_1;
 	if (has_tuning_timer) {
+		del_timer_sync(&host->tuning_timer);
 		host->flags &= ~SDHCI_NEEDS_RETUNING;
-		mod_timer(&host->tuning_timer, jiffies +
-			host->tuning_count * HZ);
 	}
 
 	if (mmc->card) {
